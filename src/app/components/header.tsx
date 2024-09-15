973e602db73ee501592d38c17fbe7d188e392ae9
valiana35 <144236658+valiana35@users.noreply.github.com>
2024-09-15 23:26:17 +0300

    avatar

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -13,7 +13,6 @@ export default function Header({ children }: HeaderProps) {
       </h1>
       <div className="w-px self-stretch bg-gray-300" />
       <div className="flex gap-3">
-        <Image width={44} height={44} src="/images/avatar.png" alt="avatar" />
         <div>
           <p className="text-base	font-semibold text-gray-900">Adam Smith</p>
           <p className="text-sm	font-light text-gray-900">adamsmith@gmail.com</p>
