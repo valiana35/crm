429060c83747a603679d3a8b9af15713dee3569a
valiana35 <144236658+valiana35@users.noreply.github.com>
2024-09-24 22:38:19 +0300

    error

--- src/app/(admin)/companies/[id]/page.tsx
+++ src/app/(admin)/companies/[id]/page.tsx
@@ -8,8 +8,7 @@ export interface PageProps {
 export default function Page({ params }: PageProps) {
   return (
     <>
-      <Header>Companies (String{params.id})</Header>
-      <p>{new Date().toTimeString()}</p>
+      <Header>Companies ({params.id})</Header>
     </>
   );
 }
